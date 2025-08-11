844529d0df6ecf86c7465525481a34df3a5bb3dc
mingtai <738279910@qq.com>
2025-08-11 15:06:06 +0800

    edit defaut

--- base/api/demoProject/api_demoProject_client.py
+++ base/api/demoProject/api_demoProject_client.py
@@ -6,19 +6,20 @@ from base.api.demoProject.api_demoProject_read_config import API_DemoProject_Rea
 from base.api.demoProject.api_demoProject_db_clients import API_DemoProject_DB_Clients
 from common.httpclient.doRequest import DoRequest
 
+
 class API_DemoProject_Client(object):
-    __instance=None
-    __inited=None
+    __instance = None
+    __inited = None
 
     def __new__(cls, *args, **kwargs):
         if cls.__instance is None:
-            cls.__instance=object.__new__(cls)
+            cls.__instance = object.__new__(cls)
         return cls.__instance
 
-    def __init__(self,config_file_path:str=None,env:str=None):
+    def __init__(self, config_file_path: str = None, env: str = None):
         if self.__inited is None:
-            self.demoProjectConfig=API_DemoProject_Read_Config(config_file_path,env).config
-            self.demoProjectDBClients=API_DemoProject_DB_Clients()
-            self.doRequest=DoRequest(self.demoProjectConfig.url)
+            self.demoProjectConfig = API_DemoProject_Read_Config(config_file_path, env).config
+            self.demoProjectDBClients = API_DemoProject_DB_Clients()
+            self.doRequest = DoRequest(self.demoProjectConfig.url)
 
-            self.__inited=True
+            self.__inited = True
